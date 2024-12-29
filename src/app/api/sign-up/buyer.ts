13e217341d99186f442c76df0e7147561dc95eb3
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2024-12-29 20:43:12 +0900

    fix: cover unique email error

--- src/app/api/sign-up/buyer.ts
+++ src/app/api/sign-up/buyer.ts
@@ -2,6 +2,7 @@ import { db } from "@/lib/db";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { z } from "zod";
 import bcrypt from "bcrypt";
+import { Prisma } from "@prisma/client";
 
 interface ResponseData {
   message: string;
@@ -40,6 +41,13 @@ const post = async (
   } catch (error) {
     if (error instanceof z.ZodError) {
       res.status(422).json({ message: "invalid form" });
+    }
+
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      // The .code property can be accessed in a type-safe manner
+      if (error.code === "P2002") {
+        res.status(409).json({ message: "this email is already in use" });
+      }
     }
 
     res.status(500).json({ message: "unexpected server error" });

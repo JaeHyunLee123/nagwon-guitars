d4b7cab02691259583598b552c40866686d1e0f4
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2024-12-29 17:54:03 +0900

    feat: buyer sign up frontend

---
+++ src/app/sign-up/buyer/page.tsx
@@ -0,0 +1,139 @@
+"use client";
+
+import { Button } from "@/components/ui/Button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/Form";
+import { Input } from "@/components/ui/Input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const signupFormSchema = z
+  .object({
+    email: z.string().email("이메일 포맷에 맞게 작성해주세요."),
+    password: z.string().min(10, "비밀번호는 최소 10글자 이상이여야 합니다."),
+    passwordConfirmation: z.string(),
+    name: z.string().min(2, "이름은 최소 2글자 이상이여야 합니다."),
+    phoneNumber: z
+      .string()
+      .regex(/^[\d-]+$/, "형식에 맞지 않는 전화번호입니다."),
+  })
+  .refine((data) => data.password === data.passwordConfirmation, {
+    message: "비밀번호가 서로 같지 않습니다.",
+    path: ["passwordConfirmation"], // Error will be associated with passwordConfirmation
+  });
+
+const SignUp = () => {
+  const signUpForm = useForm<z.infer<typeof signupFormSchema>>({
+    resolver: zodResolver(signupFormSchema),
+    mode: "onChange",
+  });
+
+  const onSubmit = (values: z.infer<typeof signupFormSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <main>
+      <div className="w-full flex flex-col space-y-2 items-center">
+        <span className="text-xl font-bold">회원가입</span>
+        <span className="text-sm">
+          판매자 회원 회원가입은 아래 버튼을 클릭해주세요.
+        </span>
+        <Link href="/sign-up/seller">
+          <Button variant={"outline"} className="text-xs">
+            판매자 회원가입
+          </Button>
+        </Link>
+      </div>
+
+      <Form {...signUpForm}>
+        <form
+          onSubmit={signUpForm.handleSubmit(onSubmit)}
+          className="flex flex-col justify-center items-center space-y-2 w-[90%] max-w-[25rem] border border-1 rounded-md m-4 p-4"
+        >
+          <FormField
+            control={signUpForm.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem className="w-[90%]">
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="example@example.com"
+                    type={"email"}
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={signUpForm.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem className="w-[90%]">
+                <FormLabel>비밀번호</FormLabel>
+                <FormControl>
+                  <Input type={"password"} {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={signUpForm.control}
+            name="passwordConfirmation"
+            render={({ field }) => (
+              <FormItem className="w-[90%]">
+                <FormLabel>비밀번호 확인</FormLabel>
+                <FormControl>
+                  <Input type={"password"} {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={signUpForm.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem className="w-[90%]">
+                <FormLabel>성함</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={signUpForm.control}
+            name="phoneNumber"
+            render={({ field }) => (
+              <FormItem className="w-[90%]">
+                <FormLabel>핸드폰 번호</FormLabel>
+                <FormControl>
+                  <Input {...field} type={"tel"} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">회원가입</Button>
+        </form>
+      </Form>
+    </main>
+  );
+};
+
+export default SignUp;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,14 +2,24 @@
 
 import { useState } from "react";
 import { Button } from "./ui/Button";
+import Link from "next/link";
 
 const Header = () => {
   const [isLoggedIn] = useState(false);
 
   return (
     <header className="flex justify-between py-4 px-2 border-b">
-      <span className="font-bold text-2xl">낙원기타</span>
-      {isLoggedIn ? <Button>마이 페이지</Button> : <Button>회원가입</Button>}
+      <Link href={"/"}>
+        <span className="font-bold text-2xl">낙원기타</span>
+      </Link>
+
+      {isLoggedIn ? (
+        <Button>마이 페이지</Button>
+      ) : (
+        <Link href={"/sign-up/buyer"}>
+          <Button>회원가입</Button>
+        </Link>
+      )}
     </header>
   );
 };
